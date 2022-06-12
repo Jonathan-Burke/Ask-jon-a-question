4d2f45c8449b1029bee6c57e58e1bb67e1a9a4c4
Jonathan Burke <jonathanburke747@gmail.com>
2022-06-11 20:04:24 -0400

    input is required

--- randomQ.js
+++ randomQ.js
@@ -1,9 +1,17 @@
-const randomString = ["yes", "No", "try Agian","Maybe later","I have a headache. Ask again later","Don't know. Don't care"];
+const randomString = ["yes", "No", 
+"try Agian","Maybe later",
+"I have a headache. Ask again later",
+"Don't know. Don't care"];
 // logic for Ask a question and get answer input
+
+
 function randomAnswer (){  
-      
-    document.getElementById('askQs').value  =  
-randomString [ Math.floor(Math.random()*randomString.length)];
+    let Q = document.getElementById('askQs').value 
+
+    if(Q){
+        Qa = document.getElementById('askQs').value= randomString [ Math.floor(Math.random()*randomString.length)];
+        
+    }
 
 }
 
